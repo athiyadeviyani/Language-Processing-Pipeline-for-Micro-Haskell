1c59bb6dad0c28b207e599911ec7522b364b8a57
Athiya Deviyani <athiyadee@gmail.com>
2018-10-29 20:10:54 +0000

    Update MH_Lexer.java

--- MH_Lexer.java
+++ MH_Lexer.java
@@ -40,7 +40,8 @@ static class NumAcceptor extends Acceptor implements DFA {
     int next (int state, char c) {
         switch (state) {
             case 0: if (c == '0') return 1; else if (CharTypes.isDigit(c) && (c != 0)) return 2;
-            case 1: if (CharTypes.isDigit(c)) return 2; else return 3;
+            case 1: return 3;
+            case 2: if (CharTypes.isDigit(c)) return 2; else return 3;
             default: return 3;
         }
     }
